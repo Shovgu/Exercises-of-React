82ebd150eb7219d778ed859e0dab5506c36c9801
Shovgu <114073036+Shovgu@users.noreply.github.com>
2022-11-22 14:32:39 +0400

    Add files via upload

---
+++ src/components/Header/App/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+const App=()=>{
+    return (
+        <>
+        <h3>Shovgu Mammadov</h3>
+        <p>Now I can create component</p>
+        </>
+    )
+}
+
+export default App
